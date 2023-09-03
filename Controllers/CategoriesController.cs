b7feb79fb05cfd949a6d50e5aeab651f125f8698
MaryMagdy <marry_magdy@yahoo.com>
2023-09-03 17:04:08 +0300

    Initial commit

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestRestAPI.Data;
+using TestRestAPI.Data.Models;
+
+namespace TestRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+
+        //create constarcor
+        public CategoriesController(AppDBContext db)
+        {
+            _db = db;
+        }
+        private readonly AppDBContext _db;
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategories()
+        {
+            var cats = await _db.Categories.ToListAsync();
+            return Ok(cats);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddCategory(string category)
+        {
+            Category c = new() { Name = category };
+            await _db.Categories.AddAsync(c);
+            _db.SaveChanges();
+            return Ok(c);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory([FromBody] Category category)
+        {
+            var c = await _db.Categories.SingleOrDefaultAsync(x => x.Id == category.Id);
+            if (c == null)
+            {
+                return NotFound($"Category Id {category.Id} Not found");
+            }
+            c.Name = category.Name;
+            _db.SaveChanges();
+            return Ok(c);
+        }
+    }
+}
---
+++ Data/AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestRestAPI.Data.Models;
+
+namespace TestRestAPI.Data
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options):base(options)
+        {
+                
+        }
+
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
